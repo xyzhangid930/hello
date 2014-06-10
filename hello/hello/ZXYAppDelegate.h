b0c7b3d268d67e43f35498de01eb783816af2525
xyzhangid930 <lanou3g@zhangxinyu.local>
2014-06-10 16:39:10 +0800

    1.0
    项目第一天

---
+++ hello/hello/ZXYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZXYAppDelegate.h
+//  hello
+//
+//  Created by zxy on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZXYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hello/hello/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hello
+//
+//  Created by zxy on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZXYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZXYAppDelegate class]));
+    }
+}
